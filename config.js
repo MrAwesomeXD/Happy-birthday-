e6b635ed7b1196598376844f79137a358f08daae
MrAwesomeXD <105139349+MrAwesomeXD@users.noreply.github.com>
2025-03-15 13:03:33 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'E';
+const giftUrl = 'https://www.facebook.com/joyqt.004';
+const eventName = 'Happy 16th Birthday Gungunnnnnn!!!!';
+const titleCard = '"To that special person"';
+const contentCard = 'HAPPIEST OF THE HAPPIEST BIRTHDAY to someone who has alwqys been there for me and whom I admire and care for more than anyone else! THANKS FOR EVERYTHINGGGGGG';
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
